dc3bc638af4d49475973871bcfc1dd68eb806c16
Narawit Tubtimtoe <narawittubtimtoe@192.168.1.16>
2023-12-18 18:12:56 +0700

    init

---
+++ demo_dictionary_list/demo_dictionary_list/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Dictionary<string, string> testDict = new Dictionary<string, string>();
+
+        testDict.Add("abc", "123");
+        testDict.Add("bcd", "321");
+        testDict.Add("dbc", "456");
+
+        foreach (KeyValuePair<string,string> kvp in testDict.ToList())
+        {
+            // Console.WriteLine(kvp);
+        }
+        foreach (string str in testDict.Values)
+        {
+            // Console.WriteLine(str);
+        }
+
+        // with more complex example
+        Dictionary<string, List<string>> test2DmDict = new Dictionary<string, List<string>>();
+        test2DmDict.Add("abc", new List<string>()
+        {
+            "123", "456", "978"
+        });
+        test2DmDict.Add("bcd", new List<string>()
+        {
+            "123"
+        });
+        foreach (KeyValuePair<string, List<string>> kvp in test2DmDict.ToList())
+        {
+            Console.WriteLine(kvp);
+        }
+        foreach (List<string> str in test2DmDict.Values)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
